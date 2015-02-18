646eb8e968589c5fc62f759c49338c0725682317
Allan Harper <aluhadora@gmail.com>
2015-02-17 21:01:59 -0700

    Initial Commit

---
+++ Dummy Launcher/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Dummy_Launcher
+{
+  static class Program
+  {
+    /// <summary>
+    /// The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    static void Main()
+    {
+      Process.Start(@"C:\Program Files\AutoHotkey\AutoHotkeyU64.exe", @"C:\Users\ath\Desktop\Scripts\Caffine.ahk");
+    }
+  }
+}
